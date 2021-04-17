8da15b217451c93781e232caa71c0bebbfccbcce
achaves100 <adan.chaves@outlook.com>
2021-04-17 17:03:45 +0000

    udpate

--- exercises/04-conditional-rendering/index.js
+++ exercises/04-conditional-rendering/index.js
@@ -3,21 +3,23 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const Alert = props => {
-	//add the condition inside this function
-
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+	console.log("will show the property show value: ", props.show);
+	if (props.show == false) {
+		return null;
+	} else {
+		return (
+			<div className="alert alert-danger" role="alert">
+				{text}
+			</div>
+		);
+	}
 };
+
 Alert.propTypes = {
 	color: PropTypes.string,
 	text: PropTypes.string
 };
 
-// here is where the alert component is being used, you don't have to edit this part,
-// but it helps you understand what properties are being passed to the component
 ReactDOM.render(
 	<div>
 		<Alert show={true} text="This is a primary alert-check it out!" />
