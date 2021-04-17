114a334419639efab12ad074eb1cd0943d389d2c
achaves100 <adan.chaves@outlook.com>
2021-04-17 04:43:27 +0000

    update

--- exercises/03.4-jumbotron/index.js
+++ exercises/03.4-jumbotron/index.js
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const Jumbotron = props => {
-	//here you have to return expected html using the properties being passed to the component
+	<div className="jumbotron">
+		<h1 className="display-4">{props.title}</h1>
+		<p className="lead">{props.description}</p>
+		<hr className="my-4" />
+		<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+		<p className="lead">
+			<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
+				{props.buttonLabel}
+			</a>
+		</p>
+	</div>;
 };
 
 Jumbotron.propTypes = {
-	//proptypes here
+	title: PropTypes.string,
+	description: PropTypes.string,
+	buttonLabel: PropTypes.string,
+	buttonURL: PropTypes.string
 };
 
 ReactDOM.render(
